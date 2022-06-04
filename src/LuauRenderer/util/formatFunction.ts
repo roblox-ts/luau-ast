c46e3cb74d5f61cc2832b53386908d1294bcdd6a
Rimuy <kevinntavares@gmail.com>
2022-06-04 18:54:35 -0300

    Add JSDoc parameters

--- src/LuauRenderer/util/formatFunction.ts
+++ src/LuauRenderer/util/formatFunction.ts
@@ -4,6 +4,8 @@ import { RenderState } from "LuauAST";
  * Utility function that takes a list of string values
  * and removes the space at the final of each string if the result is formattable.
  * @param state The render state.
+ * @param renderList The renderer callback.
+ * @param name The name of the function.
  */
 export function formatFunction(state: RenderState, renderList: () => ReadonlyArray<string>, name?: string) {
 	const list = renderList();
