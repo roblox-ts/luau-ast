9ec84d5da9561462a7f98d851bee37ed8b1de3a0
Rimuy <kevinntavares@gmail.com>
2022-06-05 15:07:04 -0300

    Improve renderFunction.ts

--- src/LuauRenderer/util/renderFunction.ts
+++ src/LuauRenderer/util/renderFunction.ts
@@ -6,9 +6,9 @@ import { RenderState } from "LuauAST";
  * Takes a list of string values and removes the space at the final of each string if the result is formattable.
  */
 export function renderFunction(state: RenderState, renderList: () => ReadonlyArray<string>, name?: string) {
-	const list = renderList();
-	if (state.isFormattable(`${name ?? ""}(${renderList().join(", ")})`)) {
+	const formatStr = renderList().join(", ");
+	if (state.isFormattable(`${name ?? ""}(${formatStr})`)) {
 		return `\n${state.list(renderList)}${state.indented("")}`;
 	}
-	return list.join(", ");
+	return formatStr;
 }
