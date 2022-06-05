9bb9e5337c29cd8a09c0566254b478b80b0a18d7
Rimuy <kevinntavares@gmail.com>
2022-06-05 15:03:14 -0300

    Update renderFunction.ts JSDoc info

--- src/LuauRenderer/util/renderFunction.ts
+++ src/LuauRenderer/util/renderFunction.ts
@@ -3,8 +3,7 @@ import { RenderState } from "LuauAST";
 /**
  * Renders function parameters and call arguments.
  *
- * Utility function that takes a list of string values
- * and removes the space at the final of each string if the result is formattable.
+ * Takes a list of string values and removes the space at the final of each string if the result is formattable.
  */
 export function renderFunction(state: RenderState, renderList: () => ReadonlyArray<string>, name?: string) {
 	const list = renderList();
