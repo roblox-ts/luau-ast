ee97a72ebbcafe61b3b0908335e2c9c226ba9511
Rimuy <kevinntavares@gmail.com>
2022-06-05 15:13:15 -0300

    Update renderComputedIndexExpression.ts

--- src/LuauRenderer/nodes/expressions/indexable/renderComputedIndexExpression.ts
+++ src/LuauRenderer/nodes/expressions/indexable/renderComputedIndexExpression.ts
@@ -1,11 +1,15 @@
 import luau from "LuauAST";
 import { render, RenderState } from "LuauRenderer";
-import { renderRightHandSide } from "LuauRenderer/util/renderRightHandSide";
 
 export function renderComputedIndexExpression(state: RenderState, node: luau.ComputedIndexExpression) {
 	const expStr = render(state, node.expression);
 	if (luau.isStringLiteral(node.index) && luau.isValidIdentifier(node.index.value)) {
-		return renderRightHandSide(state, `${expStr}.`, node.index);
+		const nameStr = node.index.value;
+		const formatStr = `${expStr}.${nameStr}`;
+		if (state.isFormattable(formatStr)) {
+			return `${state.newline(expStr)}${state.block(() => state.indented(`.${nameStr}`))}`;
+		}
+		return formatStr;
 	} else {
 		const indexStr = render(state, node.index);
 		return `${expStr}[${indexStr}]`;
