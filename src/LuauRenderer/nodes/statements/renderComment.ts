3c6f980542c46fa50f6a9ae14fcce232be653317
Dion <39664950+Dionysusnu@users.noreply.github.com>
2023-05-09 16:58:49 +0200

    Formatting

--- src/LuauRenderer/nodes/statements/renderComment.ts
+++ src/LuauRenderer/nodes/statements/renderComment.ts
@@ -7,9 +7,7 @@ export function renderComment(state: RenderState, node: luau.Comment) {
 	if (lines.length > 1) {
 		const eqStr = getSafeBracketEquals(node.text);
 		let result = state.line(`--[${eqStr}[`);
-		result += state.block(() =>
-			lines.map(line => state.line(line)).join(""),
-		);
+		result += state.block(() => lines.map(line => state.line(line)).join(""));
 		result += state.line(`]${eqStr}]`);
 		return result;
 	} else {
