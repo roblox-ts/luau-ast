e344fe1fe8f58118d490cecf4b0480adcad81bc1
osyrisrblx <osyrisrblx@gmail.com>
2023-10-04 21:18:47 -0400

    Break different thing

--- src/LuauRenderer/nodes/expressions/renderNumberLiteral.ts
+++ src/LuauRenderer/nodes/expressions/renderNumberLiteral.ts
@@ -2,5 +2,5 @@ import luau from "LuauAST";
 import { RenderState } from "LuauRenderer";
 
 export function renderNumberLiteral(state: RenderState, node: luau.NumberLiteral) {
-	return luau.isValidNumberLiteral(node.value) ? node.value : String(Number(node.value.replace(/_/g, "")));
+	return luau.isValidNumberLiteral(node.value) ? node.value + "0" : String(Number(node.value.replace(/_/g, "")));
 }
