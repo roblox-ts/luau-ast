4c699693c20d8af633323601a3acb6aae5ca8b3d
Rimuy <kevinntavares@gmail.com>
2024-01-14 06:35:20 -0300

    Change rendering to use forEach

--- src/LuauRenderer/nodes/expressions/renderInterpolatedString.ts
+++ src/LuauRenderer/nodes/expressions/renderInterpolatedString.ts
@@ -4,27 +4,24 @@ import { render, RenderState } from "LuauRenderer";
 const STRING_LITERAL_EDGE = '"'.length;
 
 export function renderInterpolatedString(state: RenderState, node: luau.InterpolatedString) {
-	const result = luau.list
-		.mapToArray(node.segments, expression => {
-			let expressionStr = render(state, expression);
-			if (luau.isStringLiteral(expression)) {
-				return (
-					expressionStr
-						.slice(STRING_LITERAL_EDGE, -STRING_LITERAL_EDGE)
-						// braces and newlines need to be escaped to match the TS behavior
-						.replace(/([{}])/g, "\\$1")
-						.replace(/\n/g, "\\\n")
-				);
-			}
+	let result = "";
+	luau.list.forEach(node.segments, expression => {
+		let expressionStr = render(state, expression);
+		if (luau.isStringLiteral(expression)) {
+			// braces and newlines are escaped to be valid in luau
+			result += expressionStr
+				.slice(STRING_LITERAL_EDGE, -STRING_LITERAL_EDGE)
+				.replace(/([{}])/g, "\\$1")
+				.replace(/\n/g, "\\\n");
+			return;
+		}
 
-			// `{{}}` is invalid, so we wrap it in parenthesis
-			if (luau.isTable(expression)) {
-				expressionStr = `(${expressionStr})`;
-			}
+		// `{{}}` is invalid, so we wrap it in parenthesis
+		if (luau.isTable(expression)) {
+			expressionStr = `(${expressionStr})`;
+		}
 
-			return `{${expressionStr}}`;
-		})
-		.join("");
-
+		result += `{${expressionStr}}`;
+	});
 	return `\`${result}\``;
 }
