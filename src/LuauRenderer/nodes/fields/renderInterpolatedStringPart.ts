8a361ba1abd7c0c4ca7a7c7a849450f867201c4f
Callie <71226959+chemiclast@users.noreply.github.com>
2024-03-23 15:55:40 -0500

    Fix unicode escape sequence handling in renderInterpolatedStringPart

--- src/LuauRenderer/nodes/fields/renderInterpolatedStringPart.ts
+++ src/LuauRenderer/nodes/fields/renderInterpolatedStringPart.ts
@@ -5,5 +5,8 @@ export function renderInterpolatedStringPart(state: RenderState, node: luau.Inte
 	// braces and newlines are escaped to be valid in luau
 	// () captures result, [] to search for any of: {}
 	// $1 fills in capture from original search
-	return node.text.replace(/([{}])/g, "\\$1").replace(/\n/g, "\\\n");
+	// \u{.+?} is a unicode escape sequence which should not be escaped
+	// (?<!\\u(?:{.+?)?) is a negative lookbehind for either the starting or ending brace of such a sequence
+	// to ensure the unicode escape sequence is exempt from escaping
+	return node.text.replace(/(?<!\\u(?:{.+?)?)([{}])/g, "\\$1").replace(/\n/g, "\\\n");
 }
