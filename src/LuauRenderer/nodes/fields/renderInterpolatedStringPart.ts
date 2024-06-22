a5af3f62e1ea346e1ea476bc8d2901d6a7434053
osyrisrblx <osyrisrblx@gmail.com>
2024-06-22 00:32:26 -0400

    Fix #484

--- src/LuauRenderer/nodes/fields/renderInterpolatedStringPart.ts
+++ src/LuauRenderer/nodes/fields/renderInterpolatedStringPart.ts
@@ -2,8 +2,11 @@ import luau from "LuauAST";
 import { RenderState } from "LuauRenderer";
 
 export function renderInterpolatedStringPart(state: RenderState, node: luau.InterpolatedStringPart) {
-	// braces and newlines are escaped to be valid in luau
-	// () captures result, [] to search for any of: {}
-	// $1 fills in capture from original search
-	return node.text.replace(/([{}])/g, "\\$1").replace(/(\r\n?|\n)/g, "\\$1");
+	// escape braces and newlines, but do not touch braces within unicode escape codes
+	return node.text
+		.replace(
+			/(\\u\{[0-9A-Fa-f]+\})|([{}])/g,
+			(_, unicodeEscape: string | undefined, brace: string | undefined) => unicodeEscape ?? "\\" + brace,
+		)
+		.replace(/(\r\n?|\n)/g, "\\$1");
 }
