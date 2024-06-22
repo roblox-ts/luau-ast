f7b26ebe2803931dc59285af1571cf6c6883d53f
osyrisrblx <osyrisrblx@gmail.com>
2024-06-21 23:49:03 -0400

    Fix #458

--- src/LuauRenderer/nodes/fields/renderInterpolatedStringPart.ts
+++ src/LuauRenderer/nodes/fields/renderInterpolatedStringPart.ts
@@ -5,5 +5,5 @@ export function renderInterpolatedStringPart(state: RenderState, node: luau.Inte
 	// braces and newlines are escaped to be valid in luau
 	// () captures result, [] to search for any of: {}
 	// $1 fills in capture from original search
-	return node.text.replace(/([{}])/g, "\\$1").replace(/\n/g, "\\\n");
+	return node.text.replace(/([{}])/g, "\\$1").replace(/(\r\n?|\n)/g, "\\$1");
 }
