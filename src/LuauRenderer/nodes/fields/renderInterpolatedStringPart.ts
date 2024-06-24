d59d519f2b3810708359dd775b0c9100b70f9a52
osyrisrblx <osyrisrblx@gmail.com>
2024-06-23 21:40:59 -0400

    Refactor to make things a little cleaner

--- src/LuauRenderer/nodes/fields/renderInterpolatedStringPart.ts
+++ src/LuauRenderer/nodes/fields/renderInterpolatedStringPart.ts
@@ -2,15 +2,11 @@ import luau from "LuauAST";
 import { RenderState } from "LuauRenderer";
 
 export function renderInterpolatedStringPart(state: RenderState, node: luau.InterpolatedStringPart) {
-	// escape braces and newlines, but do not touch braces within unicode escape codes
 	return (
 		node.text
-			.replace(
-				/(\\u{[0-9A-Fa-f]+})|([{}])/g,
-				(_, unicodeEscape: string | undefined, brace: string | undefined) => unicodeEscape ?? "\\" + brace,
-			)
-			// captures a CR with optionally an LF after it
-			// or just an LF on its own
+			// escape braces, but do not touch braces within unicode escape codes
+			.replace(/(\\u{[a-fA-F0-9]+})|([{}])/g, (_, unicodeEscape, brace) => unicodeEscape ?? "\\" + brace)
+			// escape newlines, captures a CR with optionally an LF after it or just an LF on its own
 			.replace(/(\r\n?|\n)/g, "\\$1")
 	);
 }
