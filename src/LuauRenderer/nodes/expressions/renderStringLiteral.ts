c9785bf41104cd9ab7224f7450c0dec221e61348
Osyris <osyrisrblx@gmail.com>
2026-09-05 14:09:00 -0400

    Add comments to needsBracketSpacing

--- src/LuauRenderer/nodes/expressions/renderStringLiteral.ts
+++ src/LuauRenderer/nodes/expressions/renderStringLiteral.ts
@@ -2,20 +2,24 @@ import luau from "LuauAST";
 import { RenderState } from "LuauRenderer";
 import { getSafeBracketEquals } from "LuauRenderer/util/getSafeBracketEquals";
 
+// separates long-bracket string delimiters from surrounding indexing brackets to avoid ambiguous syntax
 function needsBracketSpacing(node: luau.StringLiteral) {
 	const parent = node.parent;
 	if (!parent) {
 		return false;
 	}
 
+	// [ [[a]] ] = b
 	if (luau.isMapField(parent) && node === parent.index) {
 		return true;
 	}
 
+	// a[ [[b]] ]
 	if (luau.isComputedIndexExpression(parent) && node === parent.index) {
 		return true;
 	}
 
+	// { [ [[a]] ] = true }
 	if (luau.isSet(parent)) {
 		return true;
 	}
